f85d35bea479f20b40d3dde4d0b90fb4c8f1c7ac
Ebrar Islami <>
2020-01-25 20:11:18 +0100

    Add alt prop to header image

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './Footer.css';
 
-const Footer = props => {
+const Footer = () => {
 	return <div className="Footer">&copy; 2020 Ebrar Islami</div>;
 };
 
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,10 +2,10 @@ import React from 'react';
 import logo from 'assets/logo/github-logo.svg';
 import './Header.css';
 
-const Header = props => {
+const Header = () => {
 	return (
 		<div className="Header">
-			<img className="Header-Logo" src={logo} />
+			<img alt="logo" className="Header-Logo" src={logo} />
 			<h2 className="Header-Title">Github Issues</h2>
 		</div>
 	);
